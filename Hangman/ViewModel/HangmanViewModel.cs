38f0c56fbe1f78ccca4289b4e43cf32764b44407
Bolba-Mateescu Andrei-Ioan <andrei_bolba@yahoo.com>
2022-04-02 21:41:10 +0300

    Add some change to MVVM arhitecture

--- Hangman/ViewModel/HangmanViewModel.cs
+++ Hangman/ViewModel/HangmanViewModel.cs
@@ -4,11 +4,29 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Hangman.ViewModel
 {
     public class HangmanViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private ICommand m_pressLetter;
+
+        public void pressLetter(object parameter)
+        {
+            MessageBox.Show("Merge");
+        }
+
+        public ICommand PressLetter
+        {
+            get {
+                if (m_pressLetter == null)
+                    m_pressLetter = new RelayCommand(pressLetter);
+                return m_pressLetter;
+            }
+        }                                                                                                                                                                                                                             
     }
-}
+}                                                                       
